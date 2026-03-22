9cbf693e62f3b08eb6ff2570eb80f16b411680b0
UmaDraz-cmd <umardraz1159l@gmail.com>
2026-03-22 10:00:45 -0700

    commit new task

--- Revision_lab#1-4/reverse_a_vector_elements.cpp
+++ Revision_lab#1-4/reverse_a_vector_elements.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-    vector<int> reverseVector(vector<int> &vec)  {
-        vector<int> vec2(vec.size());
-        for(int i=vec.size()-1; i>=0; i--) {
-            vec2[vec.size()-1-i] = vec[i];
+    // vector<int> reverseVector(vector<int> &vec)  {
+    //     vector<int> vec2(vec.size());
+    //     for(int i=vec.size()-1; i>=0; i--) {
+    //         vec2[vec.size()-1-i] = vec[i];
+    //     }
+    //     return vec2;
+    // }
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int k=0;
+        vector<int> temp(nums1.size());
+        for(int i=0; i<nums1.size(); i++) {
+            for(int j=0; j<nums2.size(); j++) {
+                if(nums1[i]== nums2[j]) {
+                    temp[k]= nums1[i];
+                    k++;
+                }
+            }
         }
-        return vec2;
+        return temp;
+        
     }
 int main() {
     int size;
@@ -17,7 +31,15 @@ int main() {
     for (int i = 0; i < size; i++) {
         cin >> vec[i];  
     }
-    vector<int> reversedVec = reverseVector(vec);       
+    int size1;
+    cout << "Enter the size of the vector: ";       
+    cin >> size1;
+    vector<int> vec1(size1);
+    cout << "Enter the elements of the vector: ";
+    for (int i = 0; i < size1; i++) {
+        cin >> vec1[i];  
+    }
+    vector<int> reversedVec = intersection(vec,vec1);       
     cout << "The reversed vector is: ";
     for (int i = 0; i < reversedVec.size(); i++) {
         cout << reversedVec[i] << " ";
---
+++ Revision_lab#1-4/reverse_string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void reverseString(vector<char>& s) {
+        int start=0, end= s.size()-1;
+        while( start<end) {
+            swap(s[start],s[end]);
+            start++;
+            end--;
+        }
+    
+        
+    }
+int main() {
+    vector<char> = 
+}
